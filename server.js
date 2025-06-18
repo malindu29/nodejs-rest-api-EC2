4c3e652e7b495ae617dc0ab5a4f4c07f814cc62f
malindu29 <malindu1067@gmail.com>
2025-06-18 10:48:09 +0530

    new route

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ app.use("/api/products", (req, res) => {
   });
 });
 
+app.use("/api/names", (req, res) => {
+  return res.status(200).json({
+    message: "This is new feature change, a new route for names",
+  });
+});
+
 //error handler
 app.use(errorHandler);
 
